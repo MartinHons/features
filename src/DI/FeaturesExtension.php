368e8e9625dcde864a0314b76911009d0f46b95b
Martin Hons <hons46@gmail.com>
2024-08-18 21:41:44 +0200

    config changes

--- src/DI/FeaturesExtension.php
+++ src/DI/FeaturesExtension.php
@@ -15,7 +15,7 @@ class FeaturesExtension extends CompilerExtension
 	public function getConfigSchema(): Schema
 	{
 		return Expect::structure([
-			'vite_port' => Expect::string()->dynamic(),
+			'vite_port' => Expect::int()->dynamic(),
 			'build_folder' => Expect::string()->dynamic(),
 			'module_path' => Expect::string()->dynamic(),
 		]);
@@ -27,9 +27,10 @@ class FeaturesExtension extends CompilerExtension
 		$config = $this->getConfig();
 		$builder = $this->getContainerBuilder();
 
+
         $builder->addDefinition($this->prefix('myService'))
             ->setFactory(Presenter::class)
-            ->addSetup('setPort', [$config['port']]);
+            ->addSetup('setPort', [$config->port]);
 	}
 
 }
